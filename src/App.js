d694633d9df06506e923d210d3b60cc481dee86a
Himmel König <itogreniyorum@gmail.com>
2021-09-01 22:12:04 +0300

    api added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
-// import List from './components/List';
+import List from './components/List';
 import fs from './assets/fs.png';
 import aws from './assets/aws.png';
+import axios from 'axios';
 
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [img, setImg] = useState();
+  const [studentList, setStudentList] = useState([]);
+
+useEffect(() => {
+  axios
+  .get('https://jsonplaceholder.typicode.com/users')
+  .then ((res)=> setStudentList(res.data))
+
+
+},[])
 
   return (
     <div className='App'>
@@ -18,7 +28,8 @@ function App() {
       <button onClick= {()=> setImg (fs)} >FS</button>
       <button onClick= {()=> setImg (aws)} >AWS</button>
       <button onClick= {()=> setImg (0)} >Reset</button>
-
+      <hr/>
+      <List students = {studentList} />
     </div>
   )
 }
