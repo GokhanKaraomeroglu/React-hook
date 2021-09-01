142a25368f9dc690d9bd0d4e3df7595cc9d3337a
Himmel König <itogreniyorum@gmail.com>
2021-09-01 21:15:42 +0300

    Some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import cw from '../assets/cw_logo.png';
-
-const Header = () => {
-	console.log('Rendering Header Com!');
-	return(
-
-	)
-}
+import React from "react";
+import cw from "../assets/cw_logo.png";
+const Header = ({img}) => {
+  console.log("Rendering: Header Comp!");
+  return (
+    <div className="header">
+      <img
+        src={img ? img : cw}
+        alt="CW_logo"
+        style={{ margin: "1rem", maxHeight: "200px" }}
+      />
+    </div>
+  );
+};
 export default Header;
